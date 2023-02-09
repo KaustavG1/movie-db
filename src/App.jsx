88eaa3eeef6d042039c2908b52bb36924c2d4a93
kaustav <kaustav@kaustavs-MacBook-Pro.local>
2023-02-09 14:02:27 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Login from './Components/LoginPage/Login'
+import Details from './Components/DetailsPage/Details'
+import './App.css'
+
+function App() {
+
+  return (
+    <Details />
+  )
+}
+
+export default App
